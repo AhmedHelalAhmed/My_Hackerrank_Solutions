5d002985155b63c2dad129935b5c93a721f76da3
roshanjossey <roshanjossey@gmail.com>
2015-05-11 22:53:50 +0530

    Comments added for clarity

--- Contests/Pythonista/Timedate_Difference.py
+++ Contests/Pythonista/Timedate_Difference.py
@@ -1,8 +1,16 @@
-from datetime import datetime
+from datetime import datetime # To use datetime functions
 for x in range(int(input())):
-    a = input()
+    a = input() # Timestamps are taken in as strings
     b = input()
-    at = datetime.strptime(a,"%a %d %b %Y %H:%M:%S %z")
+    at = datetime.strptime(a,"%a %d %b %Y %H:%M:%S %z")# striptime function parses string and converts it into datetime object
     bt = datetime.strptime(b,"%a %d %b %Y %H:%M:%S %z")
+	'''%a maps to abbrevation of day of week(WWW format)
+	   %d maps to day (DD format)
+	   %b maps to abbrevation of month (MMM format)
+	   %Y maps to year(with century YYYY format)
+	   %H maps to hour
+	   %M maps to minute
+	   %S maps to second (I think there is a general format %T for time %H:%M:%S)
+	   %z maps to timezone(+/-HHMM format)'''
     di = abs(at - bt)
     print(di.days*24*3600 + di.seconds)
